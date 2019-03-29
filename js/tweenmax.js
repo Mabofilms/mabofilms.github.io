d53b0ed146625434d975019ff88f5d95d1dc92f6
eberling <c.eberling93@gmail.com>
2019-03-29 20:48:55 +0100

    rollout 1 no form

---
+++ js/tweenmax.js
@@ -0,0 +1,26 @@
+
+
+TweenMax.fromTo(
+    ".mabo-footer", 3, 
+    {opacity: 0, delay: 1.5}, 
+    {opacity: 0.9, delay: 1.5}
+    );
+
+/** videoheader parallax **/
+
+TweenLite.from( "#header-logo", 2.5, {y:800, opacity: 0, ease: Circ.easeOut});
+
+var parallaxScroll = new TimelineMax({paused:true});
+parallaxScroll.to( '.logo-wrapper , .header-container', 0.1, {y:-100, opacity: 0} );
+
+
+var scrollMax = $(document).height() - $(window).height();
+
+$(window).scroll( function(){
+    var curScroll = $(window).scrollTop();
+    //console.log( curScroll/scrollMax );
+    if( curScroll > 0 ){
+    parallaxScroll.progress( curScroll/scrollMax );
+    }
+})
+    
