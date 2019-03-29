c1d072f5159612da3c172cfcb07d1cd710ffbf46
eberling <c.eberling93@gmail.com>
2019-03-29 21:26:51 +0100

    minor changes

--- js/tweenmax.js
+++ js/tweenmax.js
@@ -11,8 +11,10 @@ TweenMax.fromTo(
 TweenLite.from( "#header-logo", 2.5, {y:800, opacity: 0, ease: Circ.easeOut});
 
 var parallaxScroll = new TimelineMax({paused:true});
-parallaxScroll.to( '.logo-wrapper , .header-container', 0.1, {y:-100, opacity: 0} );
-
+parallaxScroll.to( '.logo-wrapper', 0.1, {y:-500, opacity: 0} )
+    .to('.header-container', 0.1, {y:600, opacity: 0}, 0)
+    .to('.img-fluid', 0.1, {y:50}, 0); 
+ 
 
 var scrollMax = $(document).height() - $(window).height();
 
